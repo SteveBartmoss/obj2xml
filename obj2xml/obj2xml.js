d352f7219a532e5eabbb688631f6b1f70a1a70df
Steve <za19012397@zapopan.tecmm.edu.mx>
2024-09-28 17:59:47 -0600

    feat: Se agrego el primer prototipo para la creacion del xml

--- obj2xml/obj2xml.js
+++ obj2xml/obj2xml.js
@@ -1,6 +1,25 @@
 
 
 export function createXml(dataObject){
-    
-    console.log(dataObject)
+
+    let stackClose=[]
+
+    let labels = Object.keys(dataObject)
+    let values = Object.values(dataObject)
+
+    let content = ""
+
+    function parserXml(element,index,array){
+        content = `<${element}>` + content
+        content = dataObject[element] + content
+        content = `<${element}/>` + content
+    }
+
+    labels.forEach(parserXml)
+
+    console.log(content)
+
+    //console.log(labels)
+    //console.log(values)
+    //console.log(dataObject)
 }
