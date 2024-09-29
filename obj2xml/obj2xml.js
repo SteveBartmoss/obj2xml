0b695a8f36a084d8647aeab6e9c538eb58e569f3
Steve <za19012397@zapopan.tecmm.edu.mx>
2024-09-28 18:14:01 -0600

    feat: Se agrego la funcion para escribir el archivo xml

--- obj2xml/obj2xml.js
+++ obj2xml/obj2xml.js
@@ -1,4 +1,4 @@
-
+import file from 'fs'
 
 export function createXml(dataObject){
 
@@ -12,10 +12,12 @@ export function createXml(dataObject){
     function parserXml(element,index,array){
         content = `<${element}>` + content
         content = dataObject[element] + content
-        content = `<${element}/>` + content
+        content = `<${element}/>` + content + '\n'
     }
 
     labels.forEach(parserXml)
+
+    file.writeFileSync('prueba.xml',content,'utf8')
 
     console.log(content)
 
