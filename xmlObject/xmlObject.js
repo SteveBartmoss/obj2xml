11994ac1b9606c0869263b97d4763838209c9853
Tony <89054896+SteveBartmoss@users.noreply.github.com>
2026-07-10 14:20:05 -0600

    feat: Se agrego una clase para el manejo de os objetos desde un xml

---
+++ xmlObject/xmlObject.js
@@ -0,0 +1,14 @@
+
+
+export class ObjectXml {
+
+    constructor(rawData){
+        this.rawData = rawData
+        this.dataObject = null
+    }
+
+
+    #builDataObject(rawData){
+           
+    }
+}
